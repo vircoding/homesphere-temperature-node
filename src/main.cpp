1bf34f38dec01b2b5643fb3805ea5f999760ba77
vircoding <nluismi80@gmail.com>
2025-04-21 16:25:47 -0400

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,45 @@
-#include <Arduino.h>
+#include <freertos/FreeRTOS.h>
+#include <painlessMesh.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define MESH_PREFIX "HomeSphereMesh"
+#define MESH_PASSWORD "123456789"
+#define MESH_PORT 5555
+
+Scheduler scheduler;
+painlessMesh mesh;
+
+void sendMessage() {
+  String msg = "Hi from node 2 ";
+  msg += mesh.getNodeId();
+  mesh.sendBroadcast(msg);
+}
+
+void taskMeshUpdate(void* parameter) {
+  while (1) {
+    mesh.update();
+
+    // Delay - 10ms
+    vTaskDelay(pdMS_TO_TICKS(10));
+  }
+}
+
+void taskSendMsg(void* parameter) {
+  while (1) {
+    sendMessage();
+
+    vTaskDelay(pdMS_TO_TICKS(3000));
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  mesh.setDebugMsgTypes(ERROR | STARTUP);
+  mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA);
+
+  xTaskCreatePinnedToCore(taskMeshUpdate, "Mesh Update", 4096, NULL, 1, NULL,
+                          0);
+  xTaskCreatePinnedToCore(taskSendMsg, "Send Message", 4096, NULL, 1, NULL, 0);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+void loop() {}
